a12b581cf8c346ee7ebc1fba667511e222bb6d8b
Samuel Ramon <samuelrbo@gmail.com>
2019-07-16 00:07:25 -0300

    Aula 18.2

--- src/components/DoisComponents.jsx
+++ src/components/DoisComponents.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export const CompA = props =>
+const CompA = props =>
   <h1>Primeiro diz: { props.valor }</h1>
 
-export const CompB = props =>
+const CompB = props =>
   <h1>Segundo diz: { props.valor }</h1>
 
+export { CompA, CompB };
 export default CompA;
+
+// export const CompA = props =>
+//   <h1>Primeiro diz: { props.valor }</h1>
+
+// export const CompB = props =>
+//   <h1>Segundo diz: { props.valor }</h1>
+
+// export default CompA;
 
 // export default function() {
 //   return <p>Teste</p>
