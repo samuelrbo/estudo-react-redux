0632bb53cedfe91763634117a3a9425bf48a30f6
Samuel Ramon <samuelrbo@gmail.com>
2019-07-15 23:54:54 -0300

    Aula 16

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeiroComponente from './components/PrimeiroComponente';
+// import PrimeiroComponente from './components/PrimeiroComponente';
+import { CompA, CompB as B } from './components/DoisComponents';
 
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
   <div>
-    <PrimeiroComponente valor="Bom dia!" aBcD={ 2**8 } eFgH={ Math.pow(2, 8) } />
+    <CompA valor="Olá eu sou A!" />
+    <B valor="B na área!" />
+    {/* <PrimeiroComponente valor="Bom dia!" aBcD={ 2**8 } eFgH={ Math.pow(2, 8) } /> */}
   </div>  
 , elemento);
