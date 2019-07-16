4fa0e4467ce6949691034073402aa51205e8ee77
Samuel Ramon <samuelrbo@gmail.com>
2019-07-15 23:35:47 -0300

    Aula 15

---
+++ src/components/PrimeiroComponente.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function () {
+  return <h1>Primeiro Componente!</h1>;
+};
+
+// function primeiro() {
+//   return <h1>Primeiro Componente!</h1>;
+// }
+
+// export default primeiro;
